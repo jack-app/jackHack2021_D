20580b792901e3272b8fdaec2e622e97ee1e8df9
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2021-05-02 12:02:39 +0900

    setup firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAY4punfrdxReQc64n_guIFCEh8hui1vwU",
+  authDomain: "emotional-chat-app.firebaseapp.com",
+  databaseURL: "https://emotional-chat-app-default-rtdb.firebaseio.com",
+  projectId: "emotional-chat-app",
+  storageBucket: "emotional-chat-app.appspot.com",
+  messagingSenderId: "250287877418",
+  appId: "1:250287877418:web:0d40b682b7ff279215c253"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
