d54214375b010c1715561e06bc3d61054215e6f6
yutteee <yusaku_nakamura_aa@icloud.com>
2021-05-02 15:09:02 +0900

    join

--- src/components/Join.vue
+++ src/components/Join.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="joinpage">
-    部屋に入るページ
+    join your room.
     <input type="text">
-    <button>送信</button>
+    <button>done</button>
   </div>
 </template>
-
 <script>
 
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.joinpage{
+  padding-top:80px;
+  font-size:50px;
+  background-color: #e6ffe9;
+  color:#000055;
+  font-style: italic;
+}
+div{
+  padding:80px
+}
 
 </style>
